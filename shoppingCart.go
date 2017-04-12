dc01fbd95376abb01dc0c282b7bc0167ad681f9d
Anna Pazera <anna.pazera@rndity.com>
2017-04-12 14:36:43 +0200

    Shopping cart added  2

--- shoppingCart.go
+++ shoppingCart.go
@@ -2,24 +2,55 @@ package main
 
 import "fmt"
 
-func main() {
 
-	r := addProduct(Product{"kurtka", 500})
-
-fmt.Println("Zawartość koszyka: ", r)
-
-}
-
+var (
+	shoppingCart []Product
+)
 
 type Product struct {
-	name string
+	name  string
 	price int
 }
 
-func addProduct (product Product) [] Product{
-	var (
-		shoppingCart [] Product
-	)
-	shoppingCart = append( shoppingCart, product)
-	return shoppingCart
+func main() {
+
+
+	product1 := createProduct("shoes", 200)
+
+
+	product2 := createProduct("jacket", 500)
+	product3 := createProduct("T-shirt", 100)
+
+
+
+
+	addProduct(product1)
+
+	fmt.Println(shoppingCart)
+
+
+	addProduct(product2)
+
+	fmt.Println(shoppingCart)
+
+	addProduct(product3)
+
+	fmt.Println(shoppingCart)
+
+
+}
+
+
+func addProduct(product Product) {
+
+
+	shoppingCart = append(shoppingCart, product)
+
+
+}
+
+func createProduct(name string, price int) (product Product) {
+	product = Product{name, price}
+
+	return product
 }

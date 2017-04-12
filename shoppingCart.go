f8b83fda8a51d4a39ac3c4d44c649ac4327cf356
Anna Pazera <anna.pazera@rndity.com>
2017-04-12 15:51:08 +0200

    Multiple shopping cart added  and corrected

--- shoppingCart.go
+++ shoppingCart.go
@@ -2,11 +2,10 @@ package main
 
 import "fmt"
 
-
 var (
-	shoppingCartAni []Product
+	shoppingCartAni  []Product
 	shoppingCartKasi []Product
-	shoppingCart []Product
+	shoppingCart     []Product
 )
 
 type Product struct {
@@ -16,38 +15,29 @@ type Product struct {
 
 func main() {
 
-
 	product1 := createProduct("shoes", 200)
 	product2 := createProduct("jacket", 500)
 	product3 := createProduct("T-shirt", 100)
 
-
-
-
-	addProduct(product1)
+	shoppingCartAni := addProduct(product1)
 	fmt.Println("Zawartość koszyka Ani", shoppingCartAni)
 
+	shoppingCartAni = addProduct(product2)
+	fmt.Println("Zawartość koszyka Ani", shoppingCartAni)
 
-	addProduct(product2)
-	fmt.Println("Zawartość koszyka Ani",shoppingCartAni)
+	shoppingCartAni = addProduct(product3)
+	fmt.Println("Zawartość koszyka Ani", shoppingCartAni)
 
-
-	addProduct(product3)
-	fmt.Println("Zawartość koszyka Ani",shoppingCartAni)
-
-	addProduct(product3)
-	fmt.Println("Zawartość koszyka Kasi",shoppingCartKasi)
-
+	shoppingCartKasi = addProduct(product3)
+	fmt.Println("Zawartość koszyka Kasi", shoppingCartKasi)
 
 }
 
-
-func addProduct(product Product)  {
-
+func addProduct(product Product) []Product {
 
 	shoppingCart = append(shoppingCart, product)
 
-
+	return shoppingCart
 }
 
 func createProduct(name string, price int) (product Product) {
